870d295d9d0c42ac99ac5522900025d0b7b8280d
Reynaldo Espinosa III <reynaldo.espinosa.iii@gmail.com>
2018-05-21 19:18:55 -0500

    updated index view and created polls view using namespacing

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,47 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from django.template import loader
+
+from .models import Question
 
 # Create your views here.
+# django shortcut view for path 127.0.0.1:8000/polls/
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+# django view for path to question detail
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
+
+
+
+# django view that needs django.template import loader
+# and django.http import HttpResponse
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     template = loader.get_template('polls/index.html')
+#     context = {
+#         'latest_question_list': latest_question_list,
+#     }
+#     return HttpResponse(template.render(context, request))
+
+
+# code for question details for path 127.0.0.1:8000/polls/details
+# returns 404 error message if question does not exist in database
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist")
+#     return render(request, 'polls/detail.html', {'question': question})
